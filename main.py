73f54e0cd3ff33ef10752d5808ab83e2b6e2c67d
Liam Ralph <liamralph42@gmail.com>
2026-02-08 08:34:55 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2026 Liam Ralph
+# https://github.com/liam-ralph
+
+# This program, including this file, is licensed under the
+# MIT (Expat) License.
+# See LICENSE or this project's source for more information.
+# Project source: https://github.com/liam-ralph/linuxdiskmark
